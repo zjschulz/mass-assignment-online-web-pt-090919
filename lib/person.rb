3011db75cfcea454c2141ffeeeffc2a9831404d2
Zachary Schulz <zjschulz@gmail.com>
2019-10-15 23:32:49 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,6 +4,8 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
   
-  def initialize()
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
   
 end
